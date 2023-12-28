60f232b6f8480bfdbff675b92410b3173ef95905
Abhishek Mallick <mallickabhishek97@gmail.com>
2023-12-29 03:06:37 +0530

    Add Redux-Persist for state persistence

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import authReducer from './state';
+import { configurationStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import { PersistGate } from "redux-persist/integration/react";
+
+// all these imports is because all the states in the redux toolkit can be saved in the local state
+// that is even the user closes tab the information will still be stored only be cleared if cache cleared
+
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, authReducer);
+const store = configurationStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <App />
+      </PersistGate>
+    </Provider>
+
   </React.StrictMode>
 );
