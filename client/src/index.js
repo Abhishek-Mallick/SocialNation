f729f362790c8f37159f7c9427d0882260bbabd8
Abhishek Mallick <mallickabhishek97@gmail.com>
2023-12-29 03:26:06 +0530

    Add theme settings and state management to App.js and index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import authReducer from './state';
-import { configurationStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from "react-redux";
 import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 import { PersistGate } from "redux-persist/integration/react";
+import storage from 'redux-persist/lib/storage';
 
 // all these imports is because all the states in the redux toolkit can be saved in the local state
 // that is even the user closes tab the information will still be stored only be cleared if cache cleared
 
 const persistConfig = { key: "root", storage, version: 1 };
 const persistedReducer = persistReducer(persistConfig, authReducer);
-const store = configurationStore({
+const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
