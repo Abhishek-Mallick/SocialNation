d0a9be4b9fa8bb771a5c95eaea75f2e3b63f0a47
Abhishek Mallick <mallickabhishek97@gmail.com>
2024-01-01 22:57:06 +0530

    Add login form component and update login page layout

---
+++ client/src/scenes/loginPage/Form.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import { Box, Button, TextField, useMediaQuery, Typography, useTheme } from '@mui/material';
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import { Formik } from "formik";
+import * as yup from "yup";
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { setLogin } from '../../state/index';
+import Dropzone from 'react-dropzone';
+import FlexBetween from '../../components/FlexBetween';
+
+
+
+const Form = () => {
+    return (
+        <div>Form</div>
+    )
+}
+
+export default Form;
