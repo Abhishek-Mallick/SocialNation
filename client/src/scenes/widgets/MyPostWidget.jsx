1ab5435792491fd57f2940b821f3684dd93b99a6
Abhishek Mallick <mallickabhishek97@gmail.com>
2024-01-05 05:09:29 +0530

    Add image upload functionality to MyPostWidget component

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -1,6 +1,33 @@
 import react from 'react';
+import { EditOutlined, DeleteOutlined, AttachFileOutlined, GifBoxOutlined, ImageOutlined, MicOutlined, MoreHorizOutlined } from "@mui/icons-material";
+import { Box, Divider, Typography, InputBase, useTheme, Button, IconButton, useMediaQuery } from "@mui/material";
+import Dropzone from 'react-dropzone';
+import FlexBetween from '../../components/FlexBetween';
+import WidgetWrapper from '../../components/WidgetWrapper';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setPosts } from '../../state';
+const MyPostWidget = ({ picturePath }) => {
+    const dispatch = useDispatch();
+    // to identify whether picture is clicked to be dropped or not
+    const [ isImage, setIsImage ] = useState(false);
+    // for if user actually picks an image
+    const [image, setImage] = useState(null);
+    const [post, setPost] = useState("");
+    const { palette } = useTheme();
+    const { _id } = useSelector((state) => state.user);
+    const token = useSelector((state) => state.token);
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+    const mediumMain = palette.neutral.mediumMain;
+    const medium = palette.neutral.medium;
 
-const MyPostWidget = () => {
+    const handlePost = async () => {
+        const formData = new FormData();
+        formData.append("userId",_id);
+        formData.append("description",post);
+        if(image)
+            formData.append("picture",image);
+    }
     return (
         <div></div>
     )
